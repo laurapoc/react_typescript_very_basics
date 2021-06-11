78205ee440dd534dd19b16f91b89eb73209cc093
laurapoc <laura.pociuniene@gmail.com>
2021-06-11 12:29:36 +0300

    using event's syntax correctly

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,10 +1,17 @@
 const EventComponent: React.FC = () => {
-  const onChange = (e) => {
-    console.log(e);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement> ) => {
+    console.log("I'm beeing dragged", event);
   };
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        drag me
+      </div>
     </div>
   );
 };
